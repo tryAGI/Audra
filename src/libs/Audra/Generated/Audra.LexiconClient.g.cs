c3c0d84a2d0d670dcdc038aa64b233b687a57520
Konstantin S. <havendv@gmail.com>
2026-09-13 21:50:54 +0400

    feat: Updated OpenAPI spec (#48)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Audra/Generated/Audra.LexiconClient.g.cs
+++ src/libs/Audra/Generated/Audra.LexiconClient.g.cs
@@ -41,7 +41,7 @@ namespace Audra
 
         internal global::Audra.AutoSDKServerConfiguration AutoSDKServerConfiguration { get; set; } = new global::Audra.AutoSDKServerConfiguration();
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Audra.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Audra.LexiconSourceGenerationContext.Default);
 
         /// <summary>
         ///
